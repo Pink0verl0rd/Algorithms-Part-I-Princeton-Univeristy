ba88f6bb839a21b10b1efdb6c651a622ac936531
Jun Ivar Doydora <Ivardoydora@gmail.com>
2024-06-26 19:43:36 -0400

    HelloByeWorld and Setup for RandomWord

---
+++ HelloByeWorld.java
@@ -0,0 +1,20 @@
+/******************************************************************************
+ *
+ *  Prints out Hello and Goodbye using the 2 command line arguments that would be provided
+ *
+ ******************************************************************************/
+
+public class HelloByeWorld {
+
+    public static void main(String[] args) {
+
+        
+        // Combinded the names together as we use the twice anways 
+        String combinedName = args[0] + " and " + args[1];
+
+        System.out.println("Hello " + combinedName);
+
+        System.out.println("Goodbye " + combinedName);
+    }
+
+}
