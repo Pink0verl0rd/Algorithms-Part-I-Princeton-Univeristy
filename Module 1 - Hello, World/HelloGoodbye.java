231caf7c9f9c4f8f5bd316ed5c02955536c81720
Jun Ivar Doydora <Ivardoydora@gmail.com>
2024-06-26 21:10:04 -0400

    Tidy

--- Module 1 - Hello, World/HelloGoodbye.java
+++ Module 1 - Hello, World/HelloGoodbye.java
@@ -4,7 +4,7 @@
  *
  ******************************************************************************/
 
-public class HelloByeWorld {
+public class HelloGoodbye {
 
     public static void main(String[] args) {
 
