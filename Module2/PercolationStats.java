cd852c92318dc309cb3f03859961ca45cc5dbc18
Jun Ivar Doydora <Ivardoydora@gmail.com>
2024-06-27 13:03:56 -0400

    Moving Around

--- Module2/PercolationStats.java
+++ Module2/PercolationStats.java
@@ -1,10 +1,9 @@
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
-import Percolation
+
 
 public class PercolationStats{
     public static void main(String[] args) {
-        Percolation value = new Percolation();
     }
 }
