bfbc6e1ad693dca06ca4adb754ebe5f6b97928f4
Jun Ivar Doydora <Ivardoydora@gmail.com>
2024-07-04 21:52:18 -0400

    Queues Done

--- Permutation.java
+++ Permutation.java
@@ -1,3 +1,19 @@
+import edu.princeton.cs.algs4.StdIn;
+
 public class Permutation {
-    
-}
+    public static void main(String[] args){
+
+        int n = Integer.parseInt(args[0]);
+        // int n = 3;
+        RandomizedQueue<String> rq = new RandomizedQueue<>();
+
+        
+        while (!StdIn.isEmpty() ) {
+            rq.enqueue(StdIn.readString());   
+        }
+        for (int i = 0; i < n ; i++){
+            System.out.println(rq.dequeue());
+        }
+
+    }
+ }
